76194bd358b88da1a6d76345b23080c29faaa661
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-02-14 22:08:30 +0900

    :recycle: HTTP用メソッド追加

--- envs/apigateway.tf
+++ envs/apigateway.tf
@@ -16,11 +16,11 @@ resource "aws_apigatewayv2_vpc_link" "this" {
 }
 
 /************************************************************
-Regional REST API Gatway - target SSL by ACM
+Regional REST API Gatway
 ************************************************************/
-resource "aws_api_gateway_rest_api" "regional_target_ssl_by_acm" {
-  name        = "regional-rest-api-target-ssl-acm"
-  description = "Regional REST API Target SSL By ACM"
+resource "aws_api_gateway_rest_api" "no_import" {
+  name        = "regional-rest-api-no-import"
+  description = "Regional REST API No Import"
   endpoint_configuration {
     ip_address_type = "ipv4"
     types           = ["REGIONAL"]
@@ -28,14 +28,14 @@ resource "aws_api_gateway_rest_api" "regional_target_ssl_by_acm" {
   api_key_source               = "HEADER"
   disable_execute_api_endpoint = false
   tags = {
-    Name = "regional-rest-api-target-ssl-acm"
+    Name = "regional-rest-api-no-import"
   }
 }
 
 /************************************************************
 Resource Policy
 ************************************************************/
-data "aws_iam_policy_document" "regional_target_ssl_by_acm" {
+data "aws_iam_policy_document" "no_import" {
   version = "2012-10-17"
   statement {
     effect = "Allow"
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "regional_target_ssl_by_acm" {
       identifiers = ["*"]
     }
     actions   = ["execute-api:Invoke"]
-    resources = ["${aws_api_gateway_rest_api.regional_target_ssl_by_acm.execution_arn}/*"]
+    resources = ["${aws_api_gateway_rest_api.no_import.execution_arn}/*"]
     condition {
       test     = "IpAddress"
       variable = "aws:SourceIp"
@@ -53,69 +53,99 @@ data "aws_iam_policy_document" "regional_target_ssl_by_acm" {
   }
 }
 
-resource "aws_api_gateway_rest_api_policy" "resource_policy_for_regional_target_ssl_by_acm" {
-  rest_api_id = aws_api_gateway_rest_api.regional_target_ssl_by_acm.id
-  policy      = data.aws_iam_policy_document.regional_target_ssl_by_acm.json
+resource "aws_api_gateway_rest_api_policy" "no_import" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  policy      = data.aws_iam_policy_document.no_import.json
 }
 
 /************************************************************
 Resource
 ************************************************************/
-resource "aws_api_gateway_resource" "regional_target_ssl_by_acm_proxy" {
-  rest_api_id = aws_api_gateway_rest_api.regional_target_ssl_by_acm.id
-  parent_id   = aws_api_gateway_rest_api.regional_target_ssl_by_acm.root_resource_id
-  path_part   = "{proxy+}"
+resource "aws_api_gateway_resource" "no_import_http" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_rest_api.no_import.root_resource_id
+  path_part   = "http"
+}
+resource "aws_api_gateway_resource" "no_import_http_acm" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_resource.no_import_http.id
+  path_part   = "acm"
+}
+resource "aws_api_gateway_resource" "no_import_http_pca" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_resource.no_import_http.id
+  path_part   = "pca"
+}
+resource "aws_api_gateway_resource" "no_import_http_selfsigned" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_resource.no_import_http.id
+  path_part   = "selfsigned"
+}
+
+resource "aws_api_gateway_resource" "no_import_https" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_rest_api.no_import.root_resource_id
+  path_part   = "https"
+}
+resource "aws_api_gateway_resource" "no_import_https_acm" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_resource.no_import_https.id
+  path_part   = "acm"
+}
+resource "aws_api_gateway_resource" "no_import_https_pca" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_resource.no_import_https.id
+  path_part   = "pca"
+}
+resource "aws_api_gateway_resource" "no_import_https_selfsigned" {
+  rest_api_id = aws_api_gateway_rest_api.no_import.id
+  parent_id   = aws_api_gateway_resource.no_import_https.id
+  path_part   = "selfsigned"
 }
 
 /************************************************************
-Method
+Method - http/acm GET
 ************************************************************/
 ### Method Request
-resource "aws_api_gateway_method" "regional_target_ssl_by_acm_proxy_any" {
-  rest_api_id          = aws_api_gateway_rest_api.regional_target_ssl_by_acm.id
-  resource_id          = aws_api_gateway_resource.regional_target_ssl_by_acm_proxy.id
-  http_method          = "ANY"
+resource "aws_api_gateway_method" "no_import_http_acm_get" {
+  rest_api_id          = aws_api_gateway_rest_api.no_import.id
+  resource_id          = aws_api_gateway_resource.no_import_http_acm.id
+  http_method          = "GET"
   authorization        = "NONE"
   authorization_scopes = []
   authorizer_id        = null
   api_key_required     = false
   request_validator_id = null
   operation_name       = null
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
-  request_models = {}
+  request_parameters   = {}
+  request_models       = {}
 }
-
 ### Integration Request
-resource "aws_api_gateway_integration" "regional_target_ssl_by_acm_proxy_any" {
-  rest_api_id             = aws_api_gateway_rest_api.regional_target_ssl_by_acm.id
-  resource_id             = aws_api_gateway_resource.regional_target_ssl_by_acm_proxy.id
-  http_method             = aws_api_gateway_method.regional_target_ssl_by_acm_proxy_any.http_method
+resource "aws_api_gateway_integration" "no_import_http_acm_get" {
+  rest_api_id             = aws_api_gateway_rest_api.no_import.id
+  resource_id             = aws_api_gateway_resource.no_import_http_acm.id
+  http_method             = aws_api_gateway_method.no_import_http_acm_get.http_method
   type                    = "HTTP_PROXY"
   connection_type         = "VPC_LINK"
   response_transfer_mode  = "BUFFERED"
-  integration_http_method = "ANY"
+  integration_http_method = "GET"
   connection_id           = aws_apigatewayv2_vpc_link.this.id
   integration_target      = aws_lb.private_alb.arn
-  uri                     = "http://${aws_lb.private_alb.dns_name}"
+  uri                     = "http://acm.${var.domain_name}"
   timeout_milliseconds    = 29000
-  cache_key_parameters    = ["method.request.path.proxy"]
-  request_parameters = {
-    "integration.request.path.proxy" = "method.request.path.proxy"
-  }
-  content_handling     = null
-  credentials          = null
-  passthrough_behavior = "WHEN_NO_MATCH"
-  request_templates    = {}
+  cache_key_parameters    = []
+  request_parameters      = {}
+  content_handling        = null
+  credentials             = null
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  request_templates       = {}
 }
-
 ### Method Response
-resource "aws_api_gateway_method_response" "regional_target_ssl_by_acm_proxy_any" {
-  rest_api_id = aws_api_gateway_rest_api.regional_target_ssl_by_acm.id
-  resource_id = aws_api_gateway_resource.regional_target_ssl_by_acm_proxy.id
-  http_method = aws_api_gateway_method.regional_target_ssl_by_acm_proxy_any.http_method
-  status_code = "200"
+resource "aws_api_gateway_method_response" "no_import_http_acm_get" {
+  rest_api_id             = aws_api_gateway_rest_api.no_import.id
+  resource_id             = aws_api_gateway_resource.no_import_http_acm.id
+  http_method             = aws_api_gateway_method.no_import_http_acm_get.http_method
+  status_code         = "200"
   response_models = {
     "application/json" = "Empty"
   }
@@ -123,32 +153,130 @@ resource "aws_api_gateway_method_response" "regional_target_ssl_by_acm_proxy_any
 }
 
 /************************************************************
-Deployment
+Method - http/pca GET
 ************************************************************/
-resource "aws_api_gateway_deployment" "regional_target_ssl_by_acm_deployment" {
-  for_each = toset(var.deployments)
-  depends_on = [
-    aws_api_gateway_method.regional_target_ssl_by_acm_proxy_any,
-    aws_api_gateway_integration.regional_target_ssl_by_acm_proxy_any,
-    aws_api_gateway_method_response.regional_target_ssl_by_acm_proxy_any
-  ]
-  rest_api_id = aws_api_gateway_rest_api.regional_target_ssl_by_acm.id
-  description = "Deployment Version ${each.key}"
-  lifecycle {
-    create_before_destroy = true
+### Method Request
+resource "aws_api_gateway_method" "no_import_http_pca_get" {
+  rest_api_id          = aws_api_gateway_rest_api.no_import.id
+  resource_id          = aws_api_gateway_resource.no_import_http_pca.id
+  http_method          = "GET"
+  authorization        = "NONE"
+  authorization_scopes = []
+  authorizer_id        = null
+  api_key_required     = false
+  request_validator_id = null
+  operation_name       = null
+  request_parameters   = {}
+  request_models       = {}
+}
+### Integration Request
+resource "aws_api_gateway_integration" "no_import_http_pca_get" {
+  rest_api_id             = aws_api_gateway_rest_api.no_import.id
+  resource_id             = aws_api_gateway_resource.no_import_http_pca.id
+  http_method             = aws_api_gateway_method.no_import_http_pca_get.http_method
+  type                    = "HTTP_PROXY"
+  connection_type         = "VPC_LINK"
+  response_transfer_mode  = "BUFFERED"
+  integration_http_method = "GET"
+  connection_id           = aws_apigatewayv2_vpc_link.this.id
+  integration_target      = aws_lb.private_alb.arn
+  uri                     = "http://pca.${var.domain_name}"
+  timeout_milliseconds    = 29000
+  cache_key_parameters    = []
+  request_parameters      = {}
+  content_handling        = null
+  credentials             = null
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  request_templates       = {}
+}
+### Method Response
+resource "aws_api_gateway_method_response" "no_import_http_pca_get" {
+  rest_api_id             = aws_api_gateway_rest_api.no_import.id
+  resource_id             = aws_api_gateway_resource.no_import_http_pca.id
+  http_method             = aws_api_gateway_method.no_import_http_pca_get.http_method
+  status_code         = "200"
+  response_models = {
+    "application/json" = "Empty"
   }
+  response_parameters = {}
 }
 
 /************************************************************
-Stage
+Method - http/selfsigned GET
 ************************************************************/
-resource "aws_api_gateway_stage" "regional_target_ssl_by_acm_stage" {
-  rest_api_id           = aws_api_gateway_rest_api.regional_target_ssl_by_acm.id
-  deployment_id         = aws_api_gateway_deployment.regional_target_ssl_by_acm_deployment[local.deployment].id
-  stage_name            = "prod"
-  description           = "Production Stage"
-  cache_cluster_enabled = false
-  tags = {
-    Name = "prod"
-  }
+### Method Request
+resource "aws_api_gateway_method" "no_import_http_selfsigned_get" {
+  rest_api_id          = aws_api_gateway_rest_api.no_import.id
+  resource_id          = aws_api_gateway_resource.no_import_http_selfsigned.id
+  http_method          = "GET"
+  authorization        = "NONE"
+  authorization_scopes = []
+  authorizer_id        = null
+  api_key_required     = false
+  request_validator_id = null
+  operation_name       = null
+  request_parameters   = {}
+  request_models       = {}
 }
+### Integration Request
+resource "aws_api_gateway_integration" "no_import_http_selfsigned_get" {
+  rest_api_id             = aws_api_gateway_rest_api.no_import.id
+  resource_id             = aws_api_gateway_resource.no_import_http_selfsigned.id
+  http_method             = aws_api_gateway_method.no_import_http_selfsigned_get.http_method
+  type                    = "HTTP_PROXY"
+  connection_type         = "VPC_LINK"
+  response_transfer_mode  = "BUFFERED"
+  integration_http_method = "GET"
+  connection_id           = aws_apigatewayv2_vpc_link.this.id
+  integration_target      = aws_lb.private_alb.arn
+  uri                     = "http://selfsigned.${var.domain_name}"
+  timeout_milliseconds    = 29000
+  cache_key_parameters    = []
+  request_parameters      = {}
+  content_handling        = null
+  credentials             = null
+  passthrough_behavior    = "WHEN_NO_MATCH"
+  request_templates       = {}
+}
+### Method Response
+resource "aws_api_gateway_method_response" "no_import_http_selfsigned_get" {
+  rest_api_id             = aws_api_gateway_rest_api.no_import.id
+  resource_id             = aws_api_gateway_resource.no_import_http_selfsigned.id
+  http_method             = aws_api_gateway_method.no_import_http_selfsigned_get.http_method
+  status_code         = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {}
+}
+
+# /************************************************************
+# Deployment
+# ************************************************************/
+# resource "aws_api_gateway_deployment" "no_import_deployment" {
+#   for_each = toset(var.deployments)
+#   depends_on = [
+#     aws_api_gateway_method.no_import_proxy_any,
+#     aws_api_gateway_integration.no_import_proxy_any,
+#     aws_api_gateway_method_response.no_import_proxy_any
+#   ]
+#   rest_api_id = aws_api_gateway_rest_api.no_import.id
+#   description = "Deployment Version ${each.key}"
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+# /************************************************************
+# Stage
+# ************************************************************/
+# resource "aws_api_gateway_stage" "no_import_stage" {
+#   rest_api_id           = aws_api_gateway_rest_api.no_import.id
+#   deployment_id         = aws_api_gateway_deployment.no_import_deployment[local.deployment].id
+#   stage_name            = "prod"
+#   description           = "Production Stage"
+#   cache_cluster_enabled = false
+#   tags = {
+#     Name = "prod"
+#   }
+# }
